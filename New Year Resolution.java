3bda0d1e1c70b89d59e2cbea407904265043a468
Gayathri-252005 <gayathrideveloper25@gmail.com>
2026-06-14 18:21:46 +0530

    Create New Year Resolution.java

---
+++ New Year Resolution.java
@@ -0,0 +1,19 @@
+//Code Link : https://www.codechef.com/problems/NYRES?tab=statement
+
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		
+		Scanner sc = new Scanner(System.in);
+		int x = sc.nextInt();
+		System.out.print(x*31);
+
+	}
+}
