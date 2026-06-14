b4a33bc07b87b18ce4884d88ba907946eec34e60
Gayathri-252005 <gayathrideveloper25@gmail.com>
2026-06-14 18:39:25 +0530

    Create Odd or Even.java

---
+++ Odd or Even.java
@@ -0,0 +1,16 @@
+//Code link : https://www.geeksforgeeks.org/problems/odd-or-even3618/1?page=1&difficulty=Basic&sortBy=submissions
+
+class Solution {
+    static boolean isEven(int n) {
+        // code here
+        if(n %2 == 0)
+        {
+            return true;
+        }
+        else{
+            return false;
+        }
+        
+    }
+}
+
